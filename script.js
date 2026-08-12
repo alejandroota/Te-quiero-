0697926833c62c85b943f6a0b53fe4d2591b9815
alejandroota <alejandroortegatapia45@gmail.com>
2026-08-12 09:59:06 +0200

    Add music playback on surprise button click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const fechaSorpresa = new Date("2026-08-14T20:30:00").getTime();
 const surprise = document.getElementById("surprise");
 const surpriseButton = document.getElementById("surpriseButton");
 const letter = document.getElementById("letter");
+const music = document.getElementById("music");
 
 function comprobarFecha() {
     const ahora = new Date().getTime();
@@ -21,4 +22,6 @@ setInterval(comprobarFecha, 1000);
 surpriseButton.addEventListener("click", function () {
     surpriseButton.style.display = "none";
     letter.style.display = "block";
+
+    music.play();
 });
